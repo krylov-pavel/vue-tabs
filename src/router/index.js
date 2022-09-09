8438a107fc9423a41edfe4a5625164b60d4d6667
Pavel <krylov.pr@gmail.ru>
2022-09-09 18:25:44 +0300

    update outputDir and build project

--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/vue-tabs/' : process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    publicPath: process.env.NODE_ENV === 'production' ? '/vue-tabs/' : '/',
-    outputDir: 'docs',
     transpileDependencies: true,
 
   pluginOptions: {
